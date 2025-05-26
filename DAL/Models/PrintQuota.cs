8d761005094db7aebee0c62b49c6c3c02ef95cd8
maberchtold <berchtold.matteo@gmail.com>
2025-05-26 10:53:22 +0200

    add DAL

---
+++ DAL/Models/PrintQuota.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class PrintQuota
+    {
+        public int Id { get; set; }
+        public string? UID { get; set; }        // used by Epurse
+        public string? Username { get; set; }   // used by Faculty
+        public int NbPages { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
---
+++ DAL/PrintPaymentContext.cs
@@ -0,0 +1,34 @@
+﻿using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class PrintPaymentContext : DbContext
+    {
+        public DbSet<PrintQuota> PrintQuotas { get; set; }
+
+        public PrintPaymentContext(DbContextOptions<PrintPaymentContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            if (!builder.IsConfigured)
+            {
+                // fallback, usually unused when configured in Program.cs
+                builder.UseSqlServer("Server=192.168.1.166;Database=DigitecDB;User Id=sa;Password=Bfo12345;TrustServerCertificate=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Indexes for lookup performance
+            modelBuilder.Entity<PrintQuota>()
+                .HasIndex(p => p.UID);
+
+            modelBuilder.Entity<PrintQuota>()
+                .HasIndex(p => p.Username);
+        }
+    }
+}
